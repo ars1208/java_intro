f69a2b45ea5e8b80e332cb06f2a3f701441035e0
ars1208 <ara.kaz1208@gmail.com>
2020-05-19 13:13:12 +0900

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(3 + 2);
+        int num;
+        num = 6;
+        System.out.println(num * num);
+    }
+}
