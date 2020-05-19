3b72c43022317915f38767d12cc0edad99596fb2
ars1208 <ara.kaz1208@gmail.com>
2020-05-19 22:41:25 +0900

    add exercises

--- Main.java
+++ Main.java
@@ -5,5 +5,18 @@ public class Main {
         int num;
         num = 6;
         System.out.println(num * num);
+
+        // 練習1-2
+        int a = 3;
+        int b = 5;
+        int c = a * b;
+        System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+
+        // 練習1-3
+        boolean result = true;
+        char oneCharacter = '駆';
+        double pi = 3.14;
+        long number = 314159265853979L;
+        String attack = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
     }
 }
