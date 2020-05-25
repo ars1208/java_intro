6acd5c7dd75b09e0fa7647d1a02e7dfc6b774943
ars1208 <ara.kaz1208@gmail.com>
2020-05-25 13:18:27 +0900

    add ex8-2

--- class_ex8/src/Main.java
+++ class_ex8/src/Main.java
@@ -3,12 +3,21 @@ public class Main {
         // field
         // AirConditionerを生成
         AirConditioner ac1 = new AirConditioner();
+        AirConditioner ac2 = new AirConditioner();
 
         // method
         ac1.start();
         ac1.decreaseTargetTemp();
         ac1.decreaseTargetTemp();
 
+        ac2.start();
+        ac2.decreaseTargetTemp();
+        ac2.decreaseTargetTemp();
+        ac2.decreaseTargetTemp();
+
+        ac1.stop();
+
         System.out.println(ac1.targetTemp);
+        System.out.println(ac2.targetTemp);
     }
 }
